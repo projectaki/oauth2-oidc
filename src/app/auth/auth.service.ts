7851960beeeed95a08ea3a37707b5d5ba8b4c9a2
Akos <akosegypro@gmail.com>
2022-01-09 15:06:18 +0100

    :heavy_plus_sign: Add comments to auth service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -14,8 +14,18 @@ import { UserService } from '../user/user.service';
 export class AuthService {
   private user: string = '';
   private readonly isLoading = new BehaviorSubject(true);
+  /** Loading observable which emits false when the login is
+   * successfully completed. Initial value: true.
+   */
   public isLoading$ = this.isLoading.asObservable();
 
+  /**
+   * Observable which emits true if the user is authenticated. This observable
+   * relies on the isLoading$ observable, which means it will only emit its first value,
+   * once the authentication has successfully completed. This is neccessary in Auth guards,
+   * when all the routes have guards combined with auto login. This will prevent timing issue,
+   * infinite loop redirects.
+   */
   public isAuthenticated$ = combineLatest([
     this.isLoading$,
     this.auth.isAuthenticated$,
@@ -42,6 +52,12 @@ export class AuthService {
     private eventService: PublicEventsService
   ) {}
 
+  /**
+   * Authentication initialization method. Subscribe to it once in app component to start auth process,
+   * call backend for user info, set authenticated user state, and push false to isLoading$. If there is
+   * an error with authentication, just logout to redirect to login as there is no unprotected pages.
+   * @returns Returns the OidcSecurityService.checkAuth observable.
+   */
   initializeAuth() {
     return this.auth.checkAuth().pipe(
       switchMap((x) => this.auth.userData$),
@@ -56,6 +72,10 @@ export class AuthService {
     );
   }
 
+  /**
+   *
+   * @returns Notification event.
+   */
   getUserChangedEvent() {
     return this.eventService
       .registerForEvents()
